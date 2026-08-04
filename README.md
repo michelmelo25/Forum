4bc3070ed2ab938a1bc173aed2e79b58e2b9a21b
Michel Melo <michelmelosj25@hotmail.com>
2026-08-04 16:26:33 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Forum
+
+estudando DDD
---
+++ src/domain/use-case/answer-question.ts
@@ -0,0 +1,15 @@
+import { Answer } from "../entities/answer";
+
+interface AnswerQuestionUseCaseRequest {
+  intructorId: string;
+  questionId: string;
+  content: string;
+}
+
+export class AnswerQuestionUseCase {
+  execute({ intructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer(content);
+
+    return answer;
+  }
+}
