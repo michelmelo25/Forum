8be91fb78d4b5c8bd3a7b0cfa0f8987f9314cf00
Michel Melo <michelmelosj25@hotmail.com>
2026-08-05 13:35:18 -0300

    fundamentos-DDD ID das entidades

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,7 +1,8 @@
 import { randomUUID } from "node:crypto";
+import { uniqueEntityID } from "./unique-entity-id";
 
 export class Entity<Props> {
-  private _id: string;
+  private _id: uniqueEntityID;
   protected props: Props;
 
   get id() {
@@ -9,7 +10,7 @@ export class Entity<Props> {
   }
 
   constructor(props: Props, id?: string) {
-    this._id = id ?? randomUUID();
+    this._id = new uniqueEntityID(id);
     this.props = props;
   }
 }
