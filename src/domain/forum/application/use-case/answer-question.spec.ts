6ac6fac1514263b997bd8c5d90b89517b842b4a7
Michel Melo <michelmelosj25@hotmail.com>
2026-08-07 13:12:13 -0300

    DDD: refatorando pastas

--- src/domain/forum/application/use-case/answer-question.spec.ts
+++ src/domain/forum/application/use-case/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from "./answer-question";
 import type { AnswerRepository } from "../repositories/answer-repository";
-import type { Answer } from "../entities/answer";
+import type { Answer } from "@/domain/forum/enterprise/entities/answer";
 
 const fakeAnswerRepository: AnswerRepository = {
   create: async (answer: Answer) => {
--- src/domain/forum/application/use-case/answer-question.ts
+++ src/domain/forum/application/use-case/answer-question.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Answer } from "../entities/answer";
+
 import type { AnswerRepository } from "../repositories/answer-repository";
+import { Answer } from "../../enterprise/entities/answer";
 
 interface AnswerQuestionUseCaseRequest {
   intructorId: string;
